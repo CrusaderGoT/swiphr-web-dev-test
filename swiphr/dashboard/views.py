ca141c12984eae3b1518211f2058d5fd2d0405fa
crusader <enememeka44@gmail.com>
2024-03-15 10:19:45 +0100

    placement worked

--- swiphr/dashboard/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def dash(request):
-    if request.method == 'GET':
-        return render(request, template_name="dashboard\\index.html")
